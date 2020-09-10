41091d81cfa756319e77edc33097b22a8f8df305
✨Ranker✨ <tsiory.krishna@gmail.com>
2020-09-10 11:29:10 +0300

    Redirect to API docs when navigating to the root URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ItemsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
